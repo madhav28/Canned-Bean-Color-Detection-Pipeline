58963185efdd89a195a6311e17eb333d16faa1d1
Om Sai Krishna Madhav Lella <lellaom@dev-amd24-h200.i>
2025-07-29 23:25:09 -0400

    Updated d_score calculation

--- src/calculate_d_score.py
+++ src/calculate_d_score.py
@@ -1,7 +1,8 @@
 import pandas as pd
+import numpy as np
 
 def calculate_d_score():
     df = pd.read_csv("./output/results.csv")
-    df['D_Score'] = df['L_calib']**2+df['A_calib']**2+df['B_calib']**2
-    df['D_Score'] = 1-(df['D_Score']-100)/(600-100)
+    df['D_Score'] = np.sqrt(df['L_calib']**2+df['A_calib']**2+df['B_calib']**2)
+    df['D_Score'] = 1-(df['D_Score']-12.75)/(22.5-12.75)
     df.to_csv("./output/results.csv", index=False)
